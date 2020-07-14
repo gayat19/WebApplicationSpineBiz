1e2aad8baedc03b893f5fde8a05ea338c043ac12
unknown <gayat19>
2020-07-14 09:01:56 +0530

    Day 6

--- JavaScript/script2.js
+++ JavaScript/script2.js
@@ -2,3 +2,8 @@ function fnChangeData(){
 	var element = document.getElementById("para1");
 	element.innerHTML = element.innerHTML+"<span>Hello From Script</span>";
 }
+
+function something(){
+	document.getElementById("txtName").style.color = "Blue";
+	console.log(document.getElementById("txtName").value);
+}
